8cac701f24fa78d677a4c80cac8f2139eb50b648
neo <cjbagend@gmail.com>
2023-03-22 17:45:27 +0800

    单机并发缓存

---
+++ byteView.go
@@ -0,0 +1,19 @@
+package minicache
+
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func cloneBytes(b []byte) []byte {
+	tmp := make([]byte, len(b))
+	copy(tmp, b)
+	return tmp
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
---
+++ minicache.go
@@ -0,0 +1,73 @@
+package minicache
+
+import (
+	"fmt"
+	"log"
+	"sync"
+)
+
+type Group struct {
+	name   string
+	getter Getter
+	cache  cache
+}
+
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
+
+var (
+	mu     sync.RWMutex
+	groups = make(map[string]*Group)
+)
+
+func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
+	if getter == nil {
+		panic("nil Getter")
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	g := &Group{
+		name:   name,
+		cache:  cache{cacheBytes: cacheBytes},
+		getter: getter,
+	}
+	groups[name] = g
+	return g
+}
+
+func GetGroup(name string) *Group {
+	mu.RLock()
+	defer mu.RUnlock()
+	return groups[name]
+}
+
+func (g *Group) Get(key string) (ByteView, error) {
+	if key == "" {
+		return ByteView{}, fmt.Errorf("key is required")
+	}
+	if v, ok := g.cache.get(key); ok {
+		log.Println("[cache] hit")
+		return v, nil
+	}
+	return g.load(key)
+}
+
+func (g *Group) load(key string) (ByteView, error) {
+	return g.getLocally(key)
+}
+
+func (g *Group) getLocally(key string) (ByteView, error) {
+	bytes, error := g.getter.Get(key)
+	if error != nil {
+		return ByteView{}, error
+	}
+	value := ByteView{b: cloneBytes(bytes)}
+	g.cache.add(key, value)
+	return value, nil
+}
