8d7acb9d7f895b6a0224160c6d3a51495c4f1826
neo <cjbagend@gmail.com>
2023-03-24 10:25:01 +0800

    实现分布式节点,并提供测试脚本

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"minicache/cache_"
@@ -13,8 +14,8 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
-func main() {
-	cache_.NewGroup("scores", 2<<10, cache_.GetterFunc(
+func createGroup() *cache_.Group {
+	return cache_.NewGroup("scores", 2<<10, cache_.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -22,8 +23,51 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
-	addr := "localhost:9999"
+}
+func startCacheServer(addr string, addrs []string, cache *cache_.Group) {
 	peers := cache_.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	cache.RegisterPeers(peers)
 	log.Println("cache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
+func startAPIServer(apiAddr string, cache *cache_.Group) {
+	http.Handle("/api", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		view, err := cache.Get(key)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(view.ByteSlice())
+	}))
+	log.Println("fontend server is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "cache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	cache := createGroup()
+	if api {
+		go startAPIServer(apiAddr, cache)
+	}
+	startCacheServer(addrMap[port], []string(addrs), cache)
 }
